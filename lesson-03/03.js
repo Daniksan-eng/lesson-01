d837cb3f7bb989caf27222f1b44642c564672dc5
Aleksandr Danenkov <Danik_san@mail.ru>
2026-03-17 23:11:10 +0300

    complete lesson-03 tasks

--- lesson-03/03.js
+++ lesson-03/03.js
@@ -1,3 +1,7 @@
 // Разработайте функцию findLargest, которая принимает три числа и возвращает наибольшее из них.
 
-function findLargest() {}
+function findLargest(a,b,c) {
+    return ((a > b) && (a > c)) ? a :
+            ((b > a) && (b > c)) ? b : c;
+}
+console.log(findLargest(10,20,30));
